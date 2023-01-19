3078e6dc5ac06a79a70b1350d06da746cf447e9a
Gabriel Osorno <Gabriel.Osorno@itksquare.edu.mx>
2023-01-19 13:52:53 -0600

    Firebase config added

--- src/views/Orders/index.tsx
+++ src/views/Orders/index.tsx
@@ -35,7 +35,7 @@ const Orders: React.FC = () => {
               aria-label='Orders tabs'
             >
               <Tab label='Active' value='1' />
-              <Tab label='Delivered' value='2' />
+              <Tab label='Inactive' value='2' />
             </TabList>
           </Box>
           <TabPanel
@@ -49,13 +49,51 @@ const Orders: React.FC = () => {
               marginTop: '1vh',
             }}
           >
-            <OrderSummary orderId={9813} active={true} />
-            <OrderSummary orderId={9814} active={true} />
-            <OrderSummary orderId={9815} active={true} />
-            <OrderSummary orderId={9816} active={true} />
-            <OrderSummary orderId={9817} active={true} />
-            <OrderSummary orderId={9818} active={true} />
-            <OrderSummary orderId={9819} active={true} />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 38 # 283 X 38 Y 21, PENSIONES'}
+              clientName={'Jonathan Castillo'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 10:31 a.m.'}
+              orderId={9813}
+              orderStatus={'Pending'}
+            />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 47 # 739 X 34 Y 38, F. DE MONTEJO'}
+              clientName={'Ivanna Nouel'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 12:03 p.m.'}
+              orderId={9814}
+              orderStatus={'Pending'}
+            />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 32 # 124 X 45 Y 47, CHUBURNA'}
+              clientName={'Carlos Reyes'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 12:05 p.m.'}
+              orderId={9815}
+              orderStatus={'Pending'}
+            />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 34 # 523 X 32 Y 35, CAUCEL'}
+              clientName={'Yira Gonzalez'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 12:05 p.m.'}
+              orderId={9816}
+              orderStatus={'Pending'}
+            />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 25 # 351 X 14 Y 17, CENTRO'}
+              clientName={'Juan Fernando'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 12:07 p.m.'}
+              orderId={9817}
+              orderStatus={'Pending'}
+            />
           </TabPanel>
           <TabPanel
             value='2'
@@ -68,9 +106,33 @@ const Orders: React.FC = () => {
               marginTop: '3vh',
             }}
           >
-            <OrderSummary orderId={9809} active={false} />
-            <OrderSummary orderId={9810} active={false} />
-            <OrderSummary orderId={9811} active={false} />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 47 # 739 X 34 Y 38, F. DE MONTEJO'}
+              clientName={'Ivanna Nouel'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 10:05 a.m.'}
+              orderId={9810}
+              orderStatus={'Delivered'}
+            />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 38 # 283 X 38 Y 21, PENSIONES'}
+              clientName={'Jonathan Castillo'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 10:06 a.m.'}
+              orderId={9811}
+              orderStatus={'Delivered'}
+            />
+            <OrderSummary
+              active={true}
+              clientAddress={'C 13 # 412 X 23 Y 21, YUCALPETEN'}
+              clientName={'Juan Pablo'}
+              courierName={'Gabriel Osorno'}
+              orderDate={'19 Jan, 10:06 a.m.'}
+              orderId={9812}
+              orderStatus={'Canceled'}
+            />
           </TabPanel>
         </TabContext>
       </DefaultTemplateMenu>
