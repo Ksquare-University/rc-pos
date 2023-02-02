076ca24fde992ccf380b0b4a407befaca6759652
IvannaNouel-Ksquare <Ivanna.Nouel@itksquare.edu.mx>
2023-02-01 21:38:51 -0600

    comments

--- src/components/ButtonControl/index.tsx
+++ src/components/ButtonControl/index.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material';
 import React from 'react';
+import { Button } from '@mui/material';
 
+//interface for the props of the button
 interface Props {
   label: string;
   Icon?: React.ElementType;
@@ -9,6 +10,7 @@ interface Props {
   disabled?: boolean;
 }
 
+//component 'ButtonControl'
 const ButtonControl = (props: Props) => {
   const { label, Icon, handleClick, nameClass, disabled = false } = props;
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import reducer from './slices';
 
+/* The store is created using the 'configureStore' 
+function and the state is managed by the 'reducer' */
 const store = configureStore({
   reducer
 });
 
 export default store;
 
+//hook is defined to provide the dispatch function from the store
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
